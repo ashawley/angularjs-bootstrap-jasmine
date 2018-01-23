1b8efee5d5d2e3657d63386b16724eae31f5a9fe
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2018-01-23 16:52:18 -0500

    Add missing semis in appSpec

--- test/appSpec.js
+++ test/appSpec.js
@@ -13,17 +13,17 @@ describe('app', function() {
   });
 
   it('bootstrap', function() {
-    expect($.fn.affix).toBeDefined()
-    expect($.fn.alert).toBeDefined()
-    expect($.fn.button).toBeDefined()
-    expect($.fn.carousel).toBeDefined()
-    expect($.fn.collapse).toBeDefined()
-    expect($.fn.dropdown).toBeDefined()
-    expect($.fn.modal).toBeDefined()
-    expect($.fn.popover).toBeDefined()
-    expect($.fn.scrollspy).toBeDefined()
-    expect($.fn.tab).toBeDefined()
-    expect($.fn.tooltip).toBeDefined()
+    expect($.fn.affix).toBeDefined();
+    expect($.fn.alert).toBeDefined();
+    expect($.fn.button).toBeDefined();
+    expect($.fn.carousel).toBeDefined();
+    expect($.fn.collapse).toBeDefined();
+    expect($.fn.dropdown).toBeDefined();
+    expect($.fn.modal).toBeDefined();
+    expect($.fn.popover).toBeDefined();
+    expect($.fn.scrollspy).toBeDefined();
+    expect($.fn.tab).toBeDefined();
+    expect($.fn.tooltip).toBeDefined();
   });
 
 });
